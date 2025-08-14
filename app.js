fbd6a56b39e659ab247eac585ffdce4a7aaf343c
WBruni <wagnerbruni@terra.com.br>
2025-08-14 19:21:22 -0300

    Primeira parte do Challenge

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos = [];
+
+
+function adicionarAmigo() {
+    let campoTexto = document.getElementById("amigo"); 
+    let nomeAmigo = campoTexto.value.trim();
+    
+    if (nomeAmigo === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+    listaDeAmigos.push(nomeAmigo);
+    console.log("Lista de amigos atualizada:", listaDeAmigos);
+
+    campoTexto.value = "";
+
+    exibirListaDeAmigos();
+
+}
+
+
+function exibirListaDeAmigos() {
+    let listaHTML = document.getElementById("listaAmigos");
+    listaHTML.innerHTML = "";
+    for (let i = 0; i < listaDeAmigos.length; i++) {
+        let nomeDeAmigo = listaDeAmigos[i];
+        let itemLista = document.createElement("li");
+        itemLista.textContent = nomeDeAmigo;
+
+        listaHTML.appendChild(itemLista);
+    }
+}
+
